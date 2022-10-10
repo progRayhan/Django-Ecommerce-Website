56e27ad2a632c0fb310eba1a9b90ec86f9671b4f
Rayhan <prog.rayhan1@gmail.com>
2022-10-10 16:00:26 +0600

    Product model added

--- EcommerceWebsite/store/admin.py
+++ EcommerceWebsite/store/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from store.model.product import Product
+from store.model.category import Category
 
 # Register your models here.
 admin.site.register(Product)
+admin.site.register(Category)
--- EcommerceWebsite/store/model/product.py
+++ EcommerceWebsite/store/model/product.py
@@ -1,10 +1,12 @@
 from django.db import models
+from store.model.category import Category
 
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=50)
     price = models.IntegerField(default=0)
     description = models.CharField(max_length=200, default='')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     image = models.ImageField(upload_to='uploads/products/')
 
     def __str__(self):
