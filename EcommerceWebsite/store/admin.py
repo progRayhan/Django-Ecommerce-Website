714675d744509dcc88f05b0c3c38168d7d3c3922
Rayhan <prog.rayhan1@gmail.com>
2022-10-16 15:39:55 +0600

    configure admin panel

--- EcommerceWebsite/store/admin.py
+++ EcommerceWebsite/store/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from store.model.product import Product
 from store.model.category import Category
 
+class AdminProduct(admin.ModelAdmin):
+    list_display = ['name', 'price', 'category'] 
+
 # Register your models here.
-admin.site.register(Product)
+admin.site.register(Product, AdminProduct)
 admin.site.register(Category)
